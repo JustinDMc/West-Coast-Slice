8184df42d7b60591f14eb8a44394f5e593de6bc1
Justin <jdanielmcarthur@msn.com>
2021-08-02 12:03:02 -0700

    navbar working with about page filled out and some renders on home page

--- client/src/About.js
+++ client/src/About.js
@@ -5,7 +5,7 @@ export default function About() {
         <div>
             <h1>About</h1>
             <p>
-                Welcome to our humble pizzaria! We are a Ma and Pa restaurant, that fell in love with making pizza while visiting Southern Italy. Justin the master mind of the restaurant insuring good vibes, then we have Allen our master chef, who pours his sweat and tears into every pizza he makes insuring every man, women, and child is happy.  We pride ourselves on having fresh ingredients and a family feel while visiting our restaurant. All our pizzas are cooked in a wood-fire oven that burn at a whopping 1000 ºF constantly, meaning your pizzas are done in a Flash and hot A.F.! We also offer an open floor design for our restaurant with a patio and patio bar, rain or shine, it stays open for you enjoyment.
+                Welcome to our humble pizzaria! We are a Ma and Pa restaurant, that fell in love with making pizza while visiting Southern Italy. Justin the master mind of the restaurant insuring good vibes, then we have Allen our master chef, who pours his sweat and tears into every pizza he makes insuring every man, women, and child is happy.  We pride ourselves on having fresh ingredients and a family feel while visiting our restaurant. All our pizzas are cooked in a wood-fire oven that burn at a whopping 1000 ºF constantly, meaning your pizzas are done in a Flash and hot A.F.! We also offer an open floor design for our restaurant with a patio and patio bar, rain or shine, it stays open for your enjoyment.
             </p>
         </div>
     )
