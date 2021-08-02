78143b7505810bd3772212c98042c70e2971c707
Justin <jdanielmcarthur@msn.com>
2021-08-02 10:49:04 -0700

    rendered price and img with pizza name on home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,7 +6,12 @@ export default function Home( {currentUser, pizzaCollection} ) {
         <div style={{display: "table"}}>
             <h1>Welcome {currentUser.username}, we are on the home page.</h1>
             {pizzaCollection.map(pizza => 
-                <h2>{pizza.name}</h2>)}
+                <div style={{textAlign: 'center', border: "1px solid black", maxLength: "100px", maxHeight: "250px"}}>
+                    <img src={pizza.img_url} style={{maxLength: "100px", maxHeight: "100px"}}/>
+                    <h2>{pizza.name}</h2>
+                    <h3>${pizza.price}</h3>
+                </div>
+                )}
         </div>
     )
 }
