fcf07598ce0fe85564f885d817568cde24c8ce36
Justin <jdanielmcarthur@msn.com>
2021-08-02 15:10:01 -0700

    more home page changes, mapping out the areas of the screen we need

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,21 +5,21 @@ export default function Home( {currentUser, pizzaCollection} ) {
     return (
         <div style={{display: "flex"}} className="home-screen-div">
             {/* <h1>Welcome {currentUser.username}, we are on the home page.</h1> */}
-            <div className="pizzaContainer" style={{border: "10px solid orange", flex: "50%", textAlign: 'center' ,display: "grid", gridTemplateColumns: "repeat(2, 300px)", gridRowGap: "24px", justifyContent: "space-around", alignItems: "center", width: "100%", margin: "auto"}}>
+            <div className="pizzaContainer" style={{border: "10px solid orange", flex: "65%", textAlign: 'center' ,display: "grid", gridTemplateColumns: "repeat(2, 300px)", gridRowGap: "24px", justifyContent: "space-around", alignItems: "center", width: "100%", margin: "auto"}}>
                 {pizzaCollection.map(pizza => 
                     <div key={pizza.id} style={{textAlign: 'center', border: "3px solid black",  padding: "6px", borderRadius: "8px", margin: "auto"}}>
                         <img src={pizza.img_url} style={{maxLength: "100px", maxHeight: "100px"}}/>
                         <h2>{pizza.name}</h2>
-                        <p>{pizza.toppings.map(topping => topping.name + " ")}</p>
+                        <p>{pizza.toppings.map(topping => topping.name).join(" / ")}</p>
                         <h5>{pizza.size}</h5>
                         <h3>${pizza.price}</h3>
                         <button>Order</button>
                     </div>
                     )}
             </div>
-            <div className="pizza-form" style={{flex: "50%", textAlign: 'center', border: "10px solid orange"}}>
+            <div className="pizza-form" style={{flex: "35%", textAlign: 'center', border: "10px solid orange"}}>
                 <h1>Customize pizza</h1>
-                <form>
+                <form style={{height: "30%"}}>
                     <input type="text" placeholder="Size..."></input>
                     <br></br>
                     <br></br>
@@ -27,7 +27,17 @@ export default function Home( {currentUser, pizzaCollection} ) {
                     <br></br>
                     <br></br>
                     <input type="text" placeholder="Notes..."></input>
+                    <br></br>
+                    <br></br>
+                    <input submit type="submit" value="Order"></input>
                 </form>
+                <hr></hr>
+                <div style={{textAlign: 'center'}}>
+                    <h1>Confirmation Window</h1>  
+                    <div style={{border: "2px solid black"}}>
+                          
+                    </div>
+                </div>
             </div>
         </div>
     )
