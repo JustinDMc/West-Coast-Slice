5053d31dffaacefa5ab53a6e7cfeae361b093445
Allen <apalacios1994@gmail.com>
2021-08-04 12:57:01 -0500

    just merging

--- client/src/Orders.js
+++ client/src/Orders.js
@@ -3,17 +3,17 @@ import React from 'react'
 function Orders( {currentUser}) {
     return (
         <div style={{textAlign: 'center'}}>
-                <h1 style={{color: "white"}}>Your Order.</h1> 
+                <h1 style={{color: "white", fontSize: "50px"}}>Your Order</h1> 
         <div className="userOrder">
-                <h3>Username:</h3>
-                <h3>Address:</h3>
+                <h3 style={{fontSize: "30px"}}>Username:</h3>
+                <h3 style={{fontSize: "30px"}}>Address:</h3>
                 <br></br>
                 <br></br>
                 <br></br>
-                <h3 style={{textDecoration: 'underline'}}>Summary:</h3>
-                <p style={{fontWeight: "bold"}}>Pizza(s) you have ordered:</p>
-                <p style={{fontWeight: "bold"}}>Total amount:</p>
-                <p style={{fontWeight: "bold"}}>How long until ready:</p>
+                <h3 style={{textDecoration: 'underline', fontSize: "30px"}}>Summary:</h3>
+                <p style={{fontWeight: "bold", fontSize: "30px"}}>Pizza(s) you have ordered:</p>
+                <p style={{fontWeight: "bold", fontSize: "30px"}}>Total amount:</p>
+                <p style={{fontWeight: "bold", fontSize: "30px"}}>How long until ready:</p>
             </div>
         </div>
     )
