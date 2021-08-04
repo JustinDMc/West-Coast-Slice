47a238ac6a20f6b5a91fc4c0d982b13c1ff279ad
Allen <apalacios1994@gmail.com>
2021-08-04 17:07:59 -0500

    end of Wend

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom'
 import { NavLink } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default function Login({setCurrentUser}) {
     const history = useHistory();
@@ -33,30 +35,28 @@ export default function Login({setCurrentUser}) {
 
     return (
         <div style={{textAlign: "center", margin: "auto"}}>
-            <form onSubmit={handleSubmit} style={{backgroundColor: "white", border: '1px solid black', borderRadius: "8px", padding: "8px", width: "300px", margin: "auto"}}>
-                <h1 style={{color: "orange"}}>West Coast Slice!</h1>
+            <form className="login-form" onSubmit={handleSubmit} style={{backgroundColor: "white", border: '1px solid black', borderRadius: "8px", padding: "8px", width: "300px", margin: "auto", marginTop: "250px"}}>
+                <h1 style={{color: "#cf3f0a"}}>West Coast Slice!</h1>
                 <h2>Login</h2>
-                <input 
-                    type="text" 
+                <TextField size="small" variant="outlined" type="text" 
                     placeholder="Username"
                     name="username" 
                     value={username} 
-                    onChange={(e) => setUserName(e.target.value)}>
-                </input>
+                    onChange={(e) => setUserName(e.target.value)}/>
                 <br></br>
                 <br></br>
-                <input 
+                <TextField size="small" variant="outlined"
                     type="password" 
                     placeholder="Password"
                     name="password"
                     value={password} 
-                    onChange={(e) => setPassword(e.target.value)}>
-                </input>
+                    onChange={(e) => setPassword(e.target.value)}/>
+                    <br></br>
+                    <br></br>
+                <NavLink to="/signup" style={{fontWeight: "bold"}}>Sign Up</NavLink>
                 <br></br>
-                <NavLink to="/signup">Sign Up</NavLink>
                 <br></br>
-                <br></br>
-                <input submit type="submit" value="Log In" style={{color: "black", backgroundColor: "lightgreen"}}></input>
+                <Button variant="contained" submit type="submit" value="Log In" style={{color: "black", fontWeight: "bold"}}>Login</Button>
                 <div>{errors}</div>
             </form>
         </div>
